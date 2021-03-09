d6f058e9e373331812a3830d6e3589c6edd68e1a
Tadeu <tadeunascimento36@gmail.com>
2021-03-09 19:53:40 -0300

    Foreach

--- javascript-linguagem-basica/Foreach.js
+++ javascript-linguagem-basica/Foreach.js
@@ -1,10 +1,9 @@
 // FOREACH
-const nomes = ['José'', 'João', 'Maria', 'Marina'];
-nomes.forEach(function (nome) {
+const nomes = ['João', 'José', 'Maria', 'Marina'];
+nomes.forEach(function (nome) { 
 	console.log(nome);
 });
 let tamanho;
-// percorre a lista de nome usando o método foreach
 nomes.forEach((nome) => {
 	console.log(nome);
 	tamanho = nomes.length;
